17701d9f7b083394fb0da930c5298e33c59efda2
Stephan Zeidler <stephan@ramsalt.com>
2017-10-08 19:58:45 +0200

    Test LoginTokenGenerator

--- src/Authentication/LoginTokenGenerator.php
+++ src/Authentication/LoginTokenGenerator.php
@@ -4,36 +4,39 @@
 
 namespace Fotoweb\Authentication;
 
-class LoginTokenGenerator
-{
+/**
+ * Class LoginTokenGenerator
+ *
+ * @package Fotoweb\Authentication
+ * @see https://bitbucket.org/fotoware/phplogintokengenerator
+ */
+class LoginTokenGenerator {
 
+    private $_vp;
     private $_useForWidgets;
     private $_es;
 
-    public function __construct($encryption_secret, $useForWidgets)
-    {
+
+    public function __construct ($encryption_secret, $useForWidgets) {
         $this->_useForWidgets = $useForWidgets;
         $this->_es = $encryption_secret;
     }
 
-    public function CreateLoginToken($username)
+    public function CreateLoginToken( $username )
     {
         $startTime = gmdate('Y-m-d H:i:s', strtotime("-1 minutes"));
         $endTime = gmdate("Y-m-d H:i:s", strtotime("+30 minutes"));
         $token = sprintf('s=%s;e=%s;w=%s;u=%s;', $startTime, $endTime, $this->_useForWidgets ? 'true' : 'false', $username);
         $token .= 'm=' . $this->CreateStringMac($token) . ';';
         $utf8String = utf8_encode($token);
-
         return base64_encode($utf8String);
     }
 
-    private function CreateStringMac($data)
+    private function CreateStringMac( $data )
     {
         $data = $data . 'es=' . $this->_es;
         $dataAsUtf8 = utf8_encode($data);
         $mac = md5($dataAsUtf8, true);
-
         return base64_encode($mac);
     }
-
 }
---
+++ tests/Authentication/LoginTokenGeneratorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+use Fotoweb\Authentication\LoginTokenGenerator;
+
+class LoginTokenGeneratorTest extends TestCase
+{
+
+    public function testLoginTokenGeneration()
+    {
+        $tokenGenerator = new LoginTokenGenerator('1234567abcdef', true);
+        $loginToken = $tokenGenerator->CreateLoginToken('user1');
+        $this->assertNotEmpty($loginToken,
+          'The created login token must be non empty.');
+
+        // Decode the token to be able to validate it.
+        $decodedToken = base64_decode($loginToken);
+
+        // Remove the trailing token semicolon to be able to explode them.
+        $decodedToken = substr($decodedToken, 0, -1);
+
+        // Split up the token string into its parts.
+        $partial = explode(';', $decodedToken);
+        $tokenParts = array();
+        array_walk($partial, function($val, $key) use(&$tokenParts){
+            list($key, $value) = explode('=', $val);
+            $tokenParts[$key] = $value;
+        });
+
+        // Test if the token consists of the required parts: start and end time
+        // of the token, username and signature.
+        $this->assertArrayHasKey('s', $tokenParts, 'The token must include the start time token part.');
+        $this->assertArrayHasKey('e', $tokenParts, 'The token must include the end time token part.');
+        $this->assertArrayHasKey('u', $tokenParts, 'The token must include the user token part.');
+        $this->assertArrayHasKey('m', $tokenParts, 'The token must include the signature token part.');
+
+        // Test the parsed start date.
+        $parsedDate = date_parse($tokenParts['s']);
+        $this->assertNotFalse($parsedDate, 'The parsed start date must be not false. Otherwise the date was not valid.');
+        $this->assertEquals(0, $parsedDate['error_count'], 'The parsed start date must not return any error.');
+
+        // Test the parsed end date.
+        $parsedDate = date_parse($tokenParts['e']);
+        $this->assertNotFalse($parsedDate, 'The parsed end date must be not false. Otherwise the date was not valid.');
+        $this->assertEquals(0, $parsedDate['error_count'], 'The parsed end date must not return any error.');
+    }
+}
