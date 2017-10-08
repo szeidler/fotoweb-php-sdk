ce3385d72c58b1eb29f9069c401e1438586af885
Stephan Zeidler <stephan@ramsalt.com>
2017-10-08 19:20:37 +0200

    Test injection of a custom guzzle description

--- tests/FotowebClientTest.php
+++ tests/FotowebClientTest.php
@@ -3,6 +3,7 @@
 use Fotoweb\FotowebClient;
 use GuzzleHttp\Client;
 use GuzzleHttp\Command\ResultInterface;
+use GuzzleHttp\Command\Guzzle\Description;
 use \PHPUnit\Framework\TestCase;
 
 class FotowebClientTest extends TestCase
@@ -16,7 +17,7 @@ class FotowebClientTest extends TestCase
 
         $this->client = new FotowebClient(
           [
-            'baseUrl' => getenv('BASE_URL'),
+            'baseUrl'  => getenv('BASE_URL'),
             'apiToken' => getenv('FULLAPI_KEY'),
           ]
         );
@@ -32,12 +33,63 @@ class FotowebClientTest extends TestCase
         // Inject the custom client as configuration into the FotowebClient.
         $client = new FotowebClient(
           [
-            'client' => $custom_client,
+            'client'  => $custom_client,
             'baseUrl' => getenv('BASE_URL'),
           ]
         );
 
-        $this->assertEquals($base_uri, $client->getHttpClient()->getConfig('base_uri'), 'The FotowebClient must return the base_uri of the injected Client.');
+        $this->assertEquals($custom_client, $client->getHttpClient(),
+          'The FotowebClient must return the base_uri of the injected Client.');
+    }
+
+    public function testCustomDescriptiontFromConfig()
+    {
+        $description = new Description([
+          'baseUri'    => 'http://httpbin.org/',
+          'operations' => [
+            'testing' => [
+              'httpMethod'    => 'GET',
+              'uri'           => '/get{?foo}',
+              'responseModel' => 'getResponse',
+              'parameters'    => [
+                'foo' => [
+                  'type'     => 'string',
+                  'location' => 'uri',
+                ],
+                'bar' => [
+                  'type'     => 'string',
+                  'location' => 'query',
+                ],
+              ],
+            ],
+          ],
+          'models'     => [
+            'getResponse' => [
+              'type'                 => 'object',
+              'additionalProperties' => [
+                'location' => 'json',
+              ],
+            ],
+          ],
+        ]);
+
+        // Inject the custom description as configuration into the FotowebClient.
+        $client = new FotowebClient(
+          [
+            'description' => $description,
+            'baseUrl'     => getenv('BASE_URL'),
+            'apiToken'    => getenv('FULLAPI_KEY'),
+          ]
+        );
+
+        $this->assertEquals($description, $client->getDescription(),
+          'The description must return the injected description.');
+
+        // Our custom description doesn't provide a custom ResponseModel class,
+        // so it should fallback to Fotoweb\Response\FotowebResult.
+        $this->assertInstanceOf('Fotoweb\Response\FotowebResult',
+          $client->testing(),
+          'The response must be instance of FotowebResult.');
     }
 
     /**
@@ -67,7 +119,9 @@ class FotowebClientTest extends TestCase
     public function testGetApiDescriptor()
     {
         $response = $this->client->getApiDescriptor();
-        $this->assertInstanceOf(ResultInterface::class, $response, 'The response is not a proper Guzzle result.');
-        $this->assertEquals('/fotoweb/me', $response->getHref(), 'The response should return the href of the resource.');
+        $this->assertInstanceOf(ResultInterface::class, $response,
+          'The response is not a proper Guzzle result.');
+        $this->assertEquals('/fotoweb/me', $response->getHref(),
+          'The response should return the href of the resource.');
     }
 }
