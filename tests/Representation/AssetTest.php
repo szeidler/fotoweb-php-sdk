f6468ca6dd78b9d44549b6212667e527fe944bdb
Stephan Zeidler <stephan@ramsalt.com>
2017-10-23 17:35:56 +0200

    Add update metadata command for assets

--- tests/Representation/AssetTest.php
+++ tests/Representation/AssetTest.php
@@ -9,7 +9,7 @@ use GuzzleHttp\Command\ResultInterface;
  * Tests the Asset response model.
  *
  * @package Fotoweb\Tests\Representation
- * @see \Fotoweb\Representation\Asset
+ * @see     \Fotoweb\Representation\Asset
  */
 class AssetTest extends FotowebTestWrapper
 {
@@ -30,5 +30,28 @@ class AssetTest extends FotowebTestWrapper
           'The asset response should include a filesize.');
         $this->assertNotEmpty($response->offsetGet('previews'),
           'The asset response should have previews.');
+    }
+
+    public function testUpdateMetadata()
+    {
+        $href = getenv('ASSET_HREF');
+
+        // Append a random string, to be sure, that metadata is really updated.
+        $randomMetadataString = 'Updated metadata: ' . hash('sha256', time());
+        $metadata = [40 => ['value' => $randomMetadataString]];
+        $response = $this->client->updateMetadata(['href' => $href, 'metadata' => $metadata]);
+
+        // Check, that we get a valid updated asset response back.
+        $this->assertInstanceOf(ResultInterface::class, $response,
+          'The response is not a proper Guzzle result.');
+        $this->assertEquals($href, $response->getHref(),
+          'The response should return the href of the given resource, that was updated.');
+        $this->assertEquals($metadata, $response->offsetGet('metadata'),
+          'Updated metadat we saved.');
+
+        // Double check, that the metadata was properly updated by fetching the asset again.
+        $response = $this->client->getAsset(['href' => $href]);
+        $this->assertEquals($metadata, $response->offsetGet('metadata'),
+          'Updated metadata must match the metadata we saved.');
     }
 }
