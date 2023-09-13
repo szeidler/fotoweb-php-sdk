de3777f766a610215c27357bd81a5a0eec213cd3
Stephan Zeidler <stephan@ramsalt.com>
2023-09-13 13:59:51 +0200

    Do not verify ssl

--- tests/FotowebTestWrapper.php
+++ tests/FotowebTestWrapper.php
@@ -17,6 +17,7 @@ class FotowebTestWrapper extends TestCase
           [
             'baseUrl'  => getenv('BASE_URL'),
             'apiToken' => getenv('FULLAPI_KEY'),
+            'client_config' => ['verify' => FALSE],
           ]
         );
     }
